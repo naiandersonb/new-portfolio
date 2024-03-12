391f428049f58cee5cf154fc0c286ece7baa59a5
naiandersonb <nayanderson2014@gmail.com>
2024-03-12 16:37:52 -0300

    função de verificação do tema do app

--- src/utils/changeAppTheme.ts
+++ src/utils/changeAppTheme.ts
@@ -1,4 +1,10 @@
 export function changeAppTheme() {
   const htmlElement = document.documentElement
   htmlElement.classList.toggle('dark')
+
+  if (htmlElement.classList.contains('dark')) {
+    localStorage.setItem('@nb:app-theme', 'dark')
+  } else {
+    localStorage.setItem('@nb:app-theme', 'light')
+  }
 }
