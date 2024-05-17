8ccb0b688392ea0d5e244d1f51fdd2012d0396b0
naiandersonb <nayanderson2014@gmail.com>
2024-05-17 16:09:42 -0300

    novo card de projetos

--- src/app/components/projects/Card/index.tsx
+++ src/app/components/projects/Card/index.tsx
@@ -1,5 +1,5 @@
 import { Project } from '@/interfaces/home'
-import { Folders, Globe } from '@phosphor-icons/react'
+import { Folders, Globe, LockSimple } from '@phosphor-icons/react'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import { ModalDetails } from '../ModalDetails'
@@ -13,52 +13,41 @@ export function Card({ project }: Props) {
   const t = useTranslations('Project')
 
   return (
-    <article className="border p-2 rounded-xl border-neutral-800 flex items-center md:flex-row flex-col gap-5">
-      <section className="relative w-full cursor-pointer group aspect-video rounded-md overflow-hidden flex-1">
-        <Image
-          unoptimized
-          src={project.image}
-          alt={project.title}
-          objectFit="cover"
-          fill
-        />
+    <article className="p-4 group relative border border-neutral-800 rounded-md">
+      <h2 className="font-bold my-2 md:text-lg">{project.title}</h2>
+      <section className="relative w-full cursor-pointer aspect-video rounded-md overflow-hidden flex-1 group-hover:opacity-80">
+        <Image src={project.image} alt={project.title} objectFit="cover" fill />
+
+        {project?.isPrivate && (
+          <div className="absolute p-2 top-3 right-3 bg-neutral-50 rounded-full text-neutral-500 shadow-sm">
+            <LockSimple size={20} />
+          </div>
+        )}
       </section>
-      <section className="flex flex-col justify-between md:h-full md:py-6 py-3 md:pr-6 md:flex-1">
-        <div className="flex flex-col justify-between transition-opacity ease-in-out duration-300">
-          <h2 className="font-bold mb-4 md:text-lg">{project.title}</h2>
-          {!!project?.description && (
-            <p
-              className="md:text-[1rem] text-[0.875rem] line-clamp-3"
-              dangerouslySetInnerHTML={{
-                __html: project.description,
-              }}
-            />
+
+      <div className="flex items-center justify-between mt-3 gap-4 rounded-md">
+        <div className="flex items-center gap-4">
+          <CardLink
+            href={project.repository}
+            isDisabled={project?.isPrivate}
+            icon={<Folders size={16} />}
+          >
+            {t('repository')}
+          </CardLink>
+
+          {!!project?.demo && (
+            <CardLink href={project.demo} icon={<Globe size={16} />}>
+              Demo
+            </CardLink>
           )}
         </div>
-        <footer className="flex items-center justify-between gap-4 md:mt-0 mt-4">
-          <ModalDetails project={project}>
-            <button className="flex border uppercase text-xs font-bold p-2 rounded-full items-center gap-2 opacity-85 hover:opacity-100">
-              {t('viewMore')}
-            </button>
-          </ModalDetails>
 
-          <div className="flex items-center gap-4">
-            <CardLink
-              href={project.repository}
-              isDisabled={project?.isPrivate}
-              icon={<Folders size={16} />}
-            >
-              {t('repository')}
-            </CardLink>
-
-            {!!project?.demo && (
-              <CardLink href={project.demo} icon={<Globe size={16} />}>
-                Demo
-              </CardLink>
-            )}
-          </div>
-        </footer>
-      </section>
+        <ModalDetails project={project}>
+          <button className="flex border uppercase text-xs font-bold p-2 rounded-full items-center gap-2 opacity-85 hover:opacity-100">
+            {t('viewMore')}
+          </button>
+        </ModalDetails>
+      </div>
     </article>
   )
 }
